8617161b40a58ec4b85a7a8e9b300b8db80508b4
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-11-25 00:22:59 +0100

    fine esercizio

---
+++ script.js
@@ -0,0 +1,103 @@
+function random(){
+    let arrayRandom = [];
+    for (let i = 0; i < 5; i++) {
+        let nRandom = Math.floor(Math.random() * 100);
+        arrayRandom.push(nRandom);
+
+        // console.log(arrayRandom);
+    }
+    return arrayRandom;
+}
+console.log(random());
+
+let text = document.getElementById('text');
+text.innerHTML += random();
+
+
+// TIME----------------------------------------------------------------------
+
+// function timedText(){
+//     text = setTimeout( 3000, function(){text.classList.add('none')}, 3000);
+// }
+// timedText();
+
+
+// setTimeout (3000, set(text));
+// function set(text){
+//     text.classList.add('none');
+// }
+
+
+// function timedText(){
+//     text = setTimeout(3000, add(text));
+// }
+// timedText();
+// function add(element){element.classList.add('none');};
+
+
+// setTimeout(function(text){ 
+//     text.classList.add('none')
+//     // text.innerHTML = `id="text" class="none"`,
+// }, 3000);
+
+
+// setTimeout(
+//     text.classList.add('none'),
+//     // text.innerHTML = `id="text" class="none"`,
+// 6000);
+
+// -----------------------------------------------------------------------------
+
+// const userNumArray = [];
+// let second = 3;
+
+
+// function myTimer() {
+//     timer.innerHTML = second;
+//     if (second == 0) {
+//         clearInterval(clock);
+//         alert('tempo scaduto!');
+//         timer.innerHTML = '';
+//         document.getElementById('numeri-random').innerHTML = '';
+
+//         for (let i = 0; i < numArray.length; i++) {
+//             const num = parseInt(prompt('inserisci i numeri uno alla volta'));
+//             userNumArray.push(num);
+
+//             if (numArray.includes(userNumArray[i]) ) {
+//                 guessedNum.push(num);
+//             }
+//         }
+//         console.log(guessedNum);
+//         console.log(userNumArray);
+//     }
+//     second--;
+// }
+
+const clock = setInterval(time, 3000);
+
+function time(){
+
+    t = document.getElementById('text');
+    t.innerHTML = 'vuoto non è';
+    clearInterval(clock);
+    alert('Time out');
+
+    let nUser = [];
+
+    for (let i = 0; i < 5; i++) {
+    let n = parseInt(prompt('Inserisci il numero'));
+
+    nUser.push(n);
+
+    console.log(nUser);
+    document.getElementById("lo").innerHTML = nUser;
+    };
+
+if(random == nUser){
+    alert('You win!');
+}else{
+    alert('You loose!');
+}    
+};
+
